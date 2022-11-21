6c6f93f34e0f64fc052d157f18e9d9906344403c
snowari <pes1149@naver.com>
2022-11-21 19:14:53 +0900

    feat:routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import MainPage from './routes/mainPage';
+import LoginPage from './routes/loginPage';
 
 const App = () => {
-  console.log('react');
-  return <div>HI</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
