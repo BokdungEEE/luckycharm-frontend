98a51e984a700cf40ba344e6867c0f8e606fb32f
ChoiWonYu <phillip07290@naver.com>
2022-12-28 20:16:31 +0900

    style:global style 폰트 불러오기 삭제

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,16 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import GangwonEdu_OTFBoldA from './GangwonEdu_OTFBoldA.woff';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset};
   *, *::before, *::after {
     box-sizing: border-box;
   }
-  @font-face {
-    font-family: 'GangwonEdu_OTFBoldA';
-    src: url(${GangwonEdu_OTFBoldA}) format('woff')
-  }
+
   body{
     margin : 0;
     padding:0;
