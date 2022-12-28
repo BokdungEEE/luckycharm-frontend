c45300ca5eed3090517df7c37273c25614da5121
leeyujeong <lyj0047@naver.com>
2022-12-29 01:15:15 +0900

    feat: 프로그레스바

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/loginPage';
 import KakaoLogin from './pages/KakaoLogin';
 import Select from './pages/Select';
 import Message from './pages/Message';
 import IntroPage from './pages/IntroPage';
 import SubmitPage from './pages/SubmitPage';
+import Result from './pages/Result';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/select' element={<Select />} />
         <Route path='/message' element={<Message />} />
         <Route path='/submit' element={<SubmitPage />} />
+        <Route path='/result' element={<Result />} />
       </Routes>
     </div>
   );
